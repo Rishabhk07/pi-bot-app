e23914ffaa9457b60fd4a138a923b2119f81af29
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-05-07 13:02:29 +0530

    choice activity functionality completed
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/condingblocks/remotecar/views/ChoiceActivity.java
+++ app/src/main/java/com/condingblocks/remotecar/views/ChoiceActivity.java
@@ -1,15 +1,39 @@
 package com.condingblocks.remotecar.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
+import com.condingblocks.remotecar.MainActivity;
 import com.condingblocks.remotecar.R;
 
 public class ChoiceActivity extends AppCompatActivity {
+    CardView mountainCard;
+    CardView roadCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+        mountainCard = (CardView) findViewById(R.id.cardMountain);
+        roadCard = (CardView) findViewById(R.id.cardRoad);
+
+        mountainCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ChoiceActivity.this , Controls.class);
+                startActivity(i);
+            }
+        });
+
+        roadCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ChoiceActivity.this , MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
