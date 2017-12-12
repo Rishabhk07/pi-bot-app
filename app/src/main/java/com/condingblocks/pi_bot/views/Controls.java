387a1d05a600293c34054a397ab86295f88748c2
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-12-13 03:24:31 +0530

    getting location from socket
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/condingblocks/pi_bot/views/Controls.java
+++ app/src/main/java/com/condingblocks/pi_bot/views/Controls.java
@@ -1,13 +1,20 @@
 package com.condingblocks.pi_bot.views;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.condingblocks.pi_bot.R;
@@ -25,11 +32,12 @@ public class Controls extends AppCompatActivity implements View.OnClickListener 
     Button right;
     Button forward;
     Button backward;
-    FloatingActionButton btn;
+    FloatingActionButton mapBtn;
     io.socket.client.Socket socket;
     public static final String TAG = "Controls";
     Gson gson;
-
+    public static Double lat;
+    public static Double longi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +48,20 @@ public class Controls extends AppCompatActivity implements View.OnClickListener 
         right = (Button) findViewById(R.id.btnLeft);
         forward = (Button) findViewById(R.id.btnForward);
         backward = (Button) findViewById(R.id.btnBackward);
-        btn = (FloatingActionButton) findViewById(R.id.floating);
+        mapBtn = (FloatingActionButton) findViewById(R.id.floating);
         gson = new Gson();
-
+        getLocation();
         left.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 Log.d(TAG, "onTouch: " + event.toString());
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     disableRight();
                     disableForward();
                     disableBackward();
                     sendSocketData("left");
                     Log.d(TAG, "onTouch: Left down");
-                }else if(event.getAction() == MotionEvent.ACTION_UP){
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     enableRight();
                     enableForward();
                     enableBackward();
@@ -67,14 +75,14 @@ public class Controls extends AppCompatActivity implements View.OnClickListener 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     disableLeft();
                     disableForward();
                     disableBackward();
                     sendSocketData("right");
                     Log.d(TAG, "onTouch: right down");
 
-                }else if(event.getAction() == MotionEvent.ACTION_UP){
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     enableLeft();
                     enableForward();
                     enableBackward();
@@ -88,14 +96,14 @@ public class Controls extends AppCompatActivity implements View.OnClickListener 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     disableRight();
                     disableLeft();
                     disableBackward();
                     sendSocketData("forward");
                     Log.d(TAG, "onTouch: forward down");
 
-                }else if(event.getAction() == MotionEvent.ACTION_UP){
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     enableRight();
                     enableLeft();
                     enableBackward();
@@ -109,14 +117,14 @@ public class Controls extends AppCompatActivity implements View.OnClickListener 
         backward.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     disableRight();
                     disableForward();
                     disableLeft();
                     sendSocketData("backward");
                     Log.d(TAG, "onTouch: backward down");
 
-                }else if(event.getAction() == MotionEvent.ACTION_UP){
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     enableRight();
                     enableForward();
                     enableLeft();
@@ -127,17 +135,61 @@ public class Controls extends AppCompatActivity implements View.OnClickListener 
             }
         });
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        mapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Controls.this,MapsActivity.class);
-                startActivity(i);
+                if(lat == null){
+                    Toast.makeText(Controls.this, "Not got the location till now", Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent i = new Intent(Controls.this, MapsActivity.class);
+                    startActivity(i);
+                }
+
             }
         });
 
 
+    }
 
+    private void getLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                100,
+                10.0f,
+                new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        lat = location.getLatitude();
+                        longi = location.getLongitude();
+                    }
 
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                }
+        );
     }
 
     @Override
@@ -172,7 +224,7 @@ public class Controls extends AppCompatActivity implements View.OnClickListener 
 
     public void socketConnection(){
         try {
-            socket = IO.socket("http://192.168.43.23:8888/");
+            socket = IO.socket("http://192.168.1.5:8888/");
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
@@ -189,6 +241,17 @@ public class Controls extends AppCompatActivity implements View.OnClickListener 
                 socket.emit("foo" , "hii");
             }
         });
+
+        socket.on("location", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+//                Location l = new Gson().fromJson(String.valueOf(args) ,Location.class);
+//                lat = l.getLatitude();
+//                longi = l.getLongitude();
+//                Log.d(TAG, "call: " + args);
+            }
+        });
+
     }
 
     public void sendSocketData(String direction){
